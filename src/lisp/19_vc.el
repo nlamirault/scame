37bc67eeeec498e7717d4ba39f8934fdec9d7b2b
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-04-09 16:40:16 +0200

    Add Magit configuration

---
+++ src/lisp/19_vc.el
@@ -0,0 +1,36 @@
+;;; 19_vc.el --- Version control
+
+;; Copyright (C) Nicolas Lamirault <nicolas.lamirault@gmail.com>
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+;; THE SOFTWARE.
+
+;;; Commentary:
+
+;;; Code:
+
+
+(use-package magit
+  :bind (("C-c g s" . magit-status)
+	 ("C-c g d" . magit-pull)
+	 ("C-c g p" . magit-push)
+	 ("C-c g l" . magit-log)
+	 ("C-c g b" . magit-branch-manager)))
+
+
+(provide '19_vc)
+;;; 19_vc.el ends here
