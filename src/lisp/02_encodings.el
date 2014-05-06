38d1dd35025c702957c738bbd5565b208be00986
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-05-06 10:42:10 +0200

    FIX UTF-8 encodings

--- src/lisp/02_encodings.el
+++ src/lisp/02_encodings.el
@@ -20,9 +20,17 @@
 ;;; Code:
 
 
+;; (set-terminal-coding-system 'utf-8)
+;; (set-keyboard-coding-system 'utf-8)
+;; (set-language-environment "UTF-8")
+;; (prefer-coding-system 'utf-8)
+
+;; Set locale to UTF8
+(set-language-environment 'utf-8)
 (set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-language-environment "UTF-8")
+(setq locale-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
 
