00787b12f8b0c2919b406d32522116ff4215ff3e
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-12-24 10:12:24 +0100

    Add directory variables

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil
+  (bug-reference-url-format . "https://github.com/nlamirault/scame/issues/%s"))
+ (markdown-mode
+  (bug-reference-bug-regexp . "\\[GH-\\(?2:[[:digit:]]+\\)]")))
+ (rst-mode
+  (bug-reference-bug-regexp . "\\[GH-\\(?2:[[:digit:]]+\\)]")))
