d74443853d8f754cc9e5746b7971c131d0865b18
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2015-04-08 11:36:04 +0200

    Add keybinding for projectile perspective

--- src/scame/packages/30_dev.el
+++ src/scame/packages/30_dev.el
@@ -61,7 +61,8 @@
   :config (setq projectile-completion-system 'helm)
   :bind (("C-c p h" . helm-projectile)))
 
-(use-package persp-projectile)
+(use-package persp-projectile
+  :bind (("C-c p s w" . projectile-persp-switch-project)))
 
 (condition-case e
     (require 'es-windows)
