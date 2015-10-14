2221afea42531227cd91610a255b1d574c728278
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2015-10-14 15:06:36 +0200

    FIX Remove theme
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- src/scame/scame.el
+++ src/scame/scame.el
@@ -43,12 +43,13 @@
   (message "Load Gnus development version")
   (require 'gnus-load))
 
+(require 'f)
+(require 's)
+
 (require 'scame-io)
 (require 'scame-ui)
 (require 'scame-pkg)
 
-(require 'f)
-(require 's)
 ;; (require 'benchmark-init)
 (require 'use-package)
 
