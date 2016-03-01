fb2bd595ce445f46790f78412a4911316306b4e5
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2016-03-01 15:00:06 +0100

    Update init.el custom settings
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- src/init.el
+++ src/init.el
@@ -19,6 +19,28 @@
 
 ;;; Code:
 
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ ;; Ex:
+ ;; '(default ((t (:family "Hack" :foundry "unknown" :slant normal :weight normal :height 113 :width normal))))
+ )
+
 (mapc (lambda (path)
         (add-to-list 'load-path (concat user-emacs-directory path)))
       '("scame/" "scame/gnus"))
