df4be3f7b36c7df86fb476d7476b22cbb66966e2
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2016-08-30 12:27:22 +0200

    FIX check xwidget before load it.
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- src/scame/core/07_scame_ui.el
+++ src/scame/core/07_scame_ui.el
@@ -79,41 +79,42 @@ Return the new window for BUFFER."
 
 ;; Xwidget
 
-(require 'xwidget)
-(add-hook 'xwidget-webkit-mode-hook
-          (lambda ()
-            (define-key xwidget-webkit-mode-map [mouse-4] 'xwidget-webkit-scroll-down)
-            (define-key xwidget-webkit-mode-map [mouse-5] 'xwidget-webkit-scroll-up)
-            (define-key xwidget-webkit-mode-map (kbd "<up>") 'xwidget-webkit-scroll-down)
-            (define-key xwidget-webkit-mode-map (kbd "<down>") 'xwidget-webkit-scroll-up)
-            (define-key xwidget-webkit-mode-map (kbd "M-w") 'xwidget-webkit-copy-selection-as-kill)
-            (define-key xwidget-webkit-mode-map (kbd "C-c") 'xwidget-webkit-copy-selection-as-kill)
+(when (require 'xwidget nil 'noerror)
+  (add-hook 'xwidget-webkit-mode-hook
+            (lambda ()
+              (define-key xwidget-webkit-mode-map [mouse-4] 'xwidget-webkit-scroll-down)
+              (define-key xwidget-webkit-mode-map [mouse-5] 'xwidget-webkit-scroll-up)
+              (define-key xwidget-webkit-mode-map (kbd "<up>") 'xwidget-webkit-scroll-down)
+              (define-key xwidget-webkit-mode-map (kbd "<down>") 'xwidget-webkit-scroll-up)
+              (define-key xwidget-webkit-mode-map (kbd "M-w") 'xwidget-webkit-copy-selection-as-kill)
+              (define-key xwidget-webkit-mode-map (kbd "C-c") 'xwidget-webkit-copy-selection-as-kill)
 
-            ;; make xwidget default browser (not for now... maybe in the future!)
-            ;; (setq browse-url-browser-function (lambda (url session)
-            ;;                                     (other-window 1)
-            ;;                                     (xwidget-browse-url-no-reuse url)))
+              ;; make xwidget default browser (not for now... maybe in the future!)
+              ;; (setq browse-url-browser-function (lambda (url session)
+              ;;                                     (other-window 1)
+              ;;                                     (xwidget-browse-url-no-reuse url)))
 
-            ;; adapt webkit according to window configuration change automatically
-            ;; without this hook, every time you change your window configuration,
-            ;; you must press 'a' to adapt webkit content to new window size
-            (add-hook 'window-configuration-change-hook (lambda ()
-                                                          (when (equal major-mode 'xwidget-webkit-mode)
-                                                            (xwidget-webkit-adjust-size-dispatch))))
+              ;; adapt webkit according to window configuration change automatically
+              ;; without this hook, every time you change your window configuration,
+              ;; you must press 'a' to adapt webkit content to new window size
+              (add-hook 'window-configuration-change-hook
+                        (lambda ()
+                          (when (equal major-mode 'xwidget-webkit-mode)
+                            (xwidget-webkit-adjust-size-dispatch))))
 
-            ;; by default, xwidget reuses previous xwidget window,
-            ;; thus overriding your current website, unless a prefix argument
-            ;; is supplied
-            ;;
-            ;; This function always opens a new website in a new window
-            (defun xwidget-browse-url-no-reuse (url &optional sessoin)
-              (interactive (progn
-                             (require 'browse-url)
-                             (browse-url-interactive-arg "xwidget-webkit URL: "
-                                                         )))
-              (xwidget-webkit-browse-url url t)))
-          )
+              ;; by default, xwidget reuses previous xwidget window,
+              ;; thus overriding your current website, unless a prefix argument
+              ;; is supplied
+              ;;
+              ;; This function always opens a new website in a new window
+              (defun xwidget-browse-url-no-reuse (url &optional sessoin)
+                (interactive (progn
+                               (require 'browse-url)
+                               (browse-url-interactive-arg "xwidget-webkit URL: "
+                                                           )))
+                (xwidget-webkit-browse-url url t)))
 
+            ))
 
 
 (provide '07_scame_ui)
