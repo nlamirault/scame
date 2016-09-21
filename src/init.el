a0396c294d0455138c03a5f08f9294f7f349f3a9
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2016-09-21 11:40:39 +0200

    Cleanup init.el file
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- src/init.el
+++ src/init.el
@@ -25,22 +25,6 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- ;; Ex:
- ;; '(default ((t (:family "Hack" :foundry "unknown" :slant normal :weight normal :height 113 :width normal))))
- )
-
 (mapc (lambda (path)
         (add-to-list 'load-path (concat user-emacs-directory path)))
       '("scame/" "scame/gnus"))
