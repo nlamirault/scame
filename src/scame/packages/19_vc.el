6971d1dfdca0ce54d7f66a57f66700bce5c9f545
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2017-05-15 13:42:54 +0200

    Add Magithub
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- src/scame/packages/19_vc.el
+++ src/scame/packages/19_vc.el
@@ -33,6 +33,10 @@
 (setq magit-auto-revert-mode nil
       magit-last-seen-setup-instructions "1.4.0")
 
+(use-package magithub
+  :after magit
+  :config (magithub-feature-autoinject t))
+
 (use-package gitconfig-mode
   :ensure t
   :pin melpa
