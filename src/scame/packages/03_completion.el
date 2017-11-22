3fbb4e5deca4ba17faad27c1e3a2440105f926e0
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2017-11-22 13:26:11 +0100

    Update: completion system
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- src/scame/packages/03_completion.el
+++ src/scame/packages/03_completion.el
@@ -23,18 +23,40 @@
   :ensure t
   :pin melpa
   :init (add-hook 'after-init-hook 'global-company-mode)
+  :bind (("M-/" . company-complete)
+         ("C-c C-y" . company-yasnippet)
+         :map company-active-map
+         ("C-p" . company-select-previous)
+         ("C-n" . company-select-next)
+         ("<tab>" . company-complete-common-or-cycle)
+         ("<backtab>" . company-select-previous)
+         :map company-search-map
+         ("C-p" . company-select-previous)
+         ("C-n" . company-select-next))
+  :config (setq company-tooltip-align-annotations t
+                company-idle-delay 0.5
+                company-minimum-prefix-length 2
+                company-require-match nil
+                company-dabbrev-ignore-case nil
+                company-dabbrev-downcase nil)
   :diminish company-mode)
 
-(use-package company-shell
-  :ensure t
-  :pin melpa
-  :config (progn
-            (add-to-list 'company-backends 'company-shell)))
+(use-package company-quickhelp
+  :if (display-graphic-p)
+  :bind (:map company-active-map
+              ("M-h" . company-quickhelp-manual-begin))
+  :init (company-quickhelp-mode 1))
 
-(use-package company-emoji
-  :ensure t
-  :pin melpa
-  :config (add-to-list 'company-backends 'company-emoji))
+;; (use-package company-shell
+;;   :ensure t
+;;   :pin melpa
+;;   :config (progn
+;;             (add-to-list 'company-backends 'company-shell)))
+
+;; (use-package company-emoji
+;;   :ensure t
+;;   :pin melpa
+;;   :config (add-to-list 'company-backends 'company-emoji))
 
 
 (provide '03_completion)
