bc04908be31a6396af30d7c99f6f9039d55ac1c5
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2017-12-12 12:07:50 +0100

    Update: customization
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- src/init.el
+++ src/init.el
@@ -35,3 +35,21 @@
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(browse-url-browser-function (quote browse-url-generic))
+ '(browse-url-generic-program "/usr/bin/google-chrome")
+ '(column-number-mode t)
+ '(package-selected-packages
+   (quote
+    (notmuch-labeler notmuch all-the-icons-gnus org-jira doom-themes bbdb calfw-cal calfw-ical calfw-org calfw flyspell-correct nov adoc-mode toml-mode fountain-mode markdown-mode yaml-mode paradox sauron alert platformio-mode arduino-mode lua-mode alchemist elixir-mode jade-mode company-tern tern js3-mode web-mode company-racer flycheck-rust racer cargo rust-mode irony-eldoc flycheck-irony company-irony irony company-inf-ruby rhtml-mode rvm ruby-tools cider haskell-mode erefactor elisp-slime-nav geiser phpunit php-mode flycheck-ocaml merlin tuareg company-go go-projectile go-direx go-dlv go-rename golint go-errcheck gotest go-eldoc go-mode erlang sly-company sly pip-requirements sphinx-doc pyvenv tox company-anaconda anaconda-mode origami dumb-jump treemacs-projectile treemacs neotree eyebrowse ibuffer-projectile editorconfig gitignore-mode gitconfig-mode magit flycheck-cask flycheck-color-mode-line flycheck multi-term fish-mode terraform-mode dockerfile-mode vagrant ansible-doc ansible puppet-mode spaceline direx all-the-icons-dired imenu-anywhere ace-jump-mode which-key ivy-rich all-the-icons-ivy ivy-hydra counsel-projectile counsel company emojify-logos emojify dashboard nlinum-hl page-break-lines ace-window mode-icons all-the-icons el-init f use-package)))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight normal :height 113 :width normal)))))
